e49ccf03fc745f118c4c90be5fcf3a82df25a8c1
Niko <nrsandholm@gmail.com>
2023-02-28 11:12:43 +0200

    Add bucket deployment

--- lib/s3deployment-stack.ts
+++ lib/s3deployment-stack.ts
@@ -1,8 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class S3DeploymentStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    const bucketA = new Bucket(this, 'BucketA', {});
+
+    const bucketADeployment = new BucketDeployment(this, 'BucketADeployment', {
+      sources: [
+        Source.asset('my_content')
+      ],
+      destinationBucket: bucketA,
+    });
   }
 }
